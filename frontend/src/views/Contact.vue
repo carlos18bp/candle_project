15f0e070c95030af8c69a0643b32d9f5aeee2668
Gustavo Pérez <gustavo.perezp@colseminario.edu.co>
2024-01-30 18:53:23 -0500

    candle-project: Refactor to Header component and Contact view.

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -1,21 +1,29 @@
 <template>  
     <Header></Header>
-    <div class="mx-auto relative flex flex-col max-w-7xl px-8">
+    <div class="mx-auto relative flex flex-col">
         <img src="@/assets/images/contact/banner_1.jpg" class="w-full h-full">
-        <div class="rounded-3xl absolute bg-white flex flex-col w-1/4 p-8 top-28 left-28">
-            <p class="font-bold pb-8">Get In Touch</p>
-            <p class="font-bold pb-6">Contact</p>
-            <div class="flex">
-                <img src="@/assets/images/icons/phone_call.png" class="size-6">
-                <p class="pl-2 pb-4">+1 (252) 591-6930</p>
-            </div>
-            <div class="flex">
-                <img src="@/assets/images/icons/sharp_email.png" class="size-6">
-                <p class="pl-2 pb-4">info@hello.com</p>
-            </div>
-            <div class="flex">
-                <img src="@/assets/images/icons/location.png" class="size-6">
-                <p class="pl-2 pb-4">Charlotte | NC-SC</p>
+        <div class="rounded-3xl absolute bg-second_p flex flex-col w-1/4 p-8 top-28 left-28">
+            <h2 class="font-bold text-center text-4xl pb-8">Get In Touch</h2>
+            <h2 class="font-bold pb-6 text-4xl">Contact</h2>
+            <div class="pb-8">
+                <div class="flex items-center pb-4">
+                    <div class="flex items-center w-8">
+                        <img src="@/assets/images/icons/phone_call.png" class="size-6">
+                    </div>
+                    <p class="font-regular text-2xl pl-2">+1 (252) 591-6930</p>
+                </div>
+                <div class="flex items-center pb-4">
+                    <div class="flex items-center w-8">
+                        <img src="@/assets/images/icons/sharp_email.png" class="size-6">
+                    </div>
+                    <p class="font-regular text-2xl pl-2">info@hello.com</p>
+                </div>
+                <div class="flex items-center">
+                    <div class="flex items-center w-8">
+                        <img src="@/assets/images/icons/location.png" class="size-6">
+                    </div>
+                    <p class="font-regular text-2xl pl-2">Charlotte | NC-SC</p>
+                </div>
             </div>
             <!-- Map Container -->
             <div class="w-full h-40 relative">
@@ -23,12 +31,12 @@
                 api-key="AIzaSyD7cf5pD4u32w9k2zEnjq19nd8WlGmm-ls"
                 class="h-full"
                 :center="center"
-                :zoom="15"
+                :zoom="10"
                 >
                 <Marker :options="{ position: center }" />
                 </GoogleMap>
             </div>
-            <div class="flex justify-between pt-8">
+            <div class="flex justify-evenly pt-8">
                 <img src="@/assets/images/icons/social_media/facebook.png" class="size-9">
                 <img src="@/assets/images/icons/social_media/instagram.png" class="size-9">
                 <img src="@/assets/images/icons/social_media/twitter.png" class="size-9">
