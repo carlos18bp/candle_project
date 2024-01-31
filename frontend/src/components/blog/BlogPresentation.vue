0285ef90ae069a535e342e28bd6adedd450e10d7
carlos18bp <carlos18bp@gmail.com>
2024-01-31 00:12:01 +0000

    candle-project: Fix img media source get data.

--- frontend/src/components/blog/BlogPresentation.vue
+++ frontend/src/components/blog/BlogPresentation.vue
@@ -1,10 +1,22 @@
 <template>  
     <div class="flex flex-col">
-        <img src="@/assets/images/home/banner_1.jpg" class="rounded w-96 h-72 pb-4">
+        <img       
+          v-if="blog && blog.image_url"
+          :src="`/api/${blog.image_url}`" 
+          class="rounded w-96 h-72 pb-4">
         <p class="text-gray-400 uppercase pb-2">{{ blog.category }}</p>
-        <p>{{ blog.title }}</p>
+        <RouterLink
+          v-if="blog.id"       
+          :to="{ name: 'blog', 
+          params: { blog_id: blog.id } }" 
+          class="btn btn-primary btn-lg pb-3">
+              {{ blog.title }}
+        </RouterLink> 
         <div class="flex pt-3">
-            <img src="@/assets/images/home/banner_1.jpg" class="rounded-full size-14">
+            <img       
+              v-if="blog && blog.owner_image_url"
+              :src="`/api/${blog.image_url}`"
+              class="object-cover rounded-full size-14">
             <div class="pl-3">
                 <p>{{ blog.owner_full_name }}</p>
                 <p>{{ blog.publication_date }}</p>
@@ -14,6 +26,8 @@
 </template>
 
 <script setup>
+  import { RouterLink } from 'vue-router';
+
   const props = defineProps({
     blog: Object,
   });
