4e92015db9a2007fe94389d15e983bd51974d423
Gustavo Pérez <gustavo.perezp@colseminario.edu.co>
2024-04-10 11:13:45 -0500

    V 1.4.2

--- candle_project/settings.py
+++ candle_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vp6a@_g#vb@b@hqrxx!yti8%q6_+)1el1j!_=n38h*pw(@o38-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
